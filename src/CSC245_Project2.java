3dddbba7df85bbe5ace3616e9ee0a14921cb2850
Toehrlein27 <kaptaunkrunch27@gmail.com>
2022-02-28 22:15:08 -0700

    Mitigation Changes: ERROR01-J and FIO16-J

--- src/CSC245_Project2.java
+++ src/CSC245_Project2.java
@@ -1,19 +1,26 @@
-//Tim Oehrlein
-//Secure Software Development
-//Program to identify and mitigate several possible errors
-
-import java.io.*;       //Importing all the java.io classes
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Map;
+import java.text.Normalizer;
 
 public class CSC245_Project2 {
+    // Read the filename from the command line argument
+    public static void main(String[] args) throws Exception {
 
-    public static void main(String[] args) {
-        // Read the filename from the command line argument
-        String fileName = args[0];                 //assigning fileName to argument 0
-        BufferedReader inputStream;         //buffered reader class
-        File file;                          //File class
-        String fileLine;        //String variable to read through the file
+        String filename = System.getenv("FILE_PATH");
+        if (filename == null) {
+            throw new Exception(".env file not found or Invalid Key");
+        }
+        BufferedReader inputStream = null;
+
+        File file;
+
+        String fileLine;
 
         try {
+            String fileName = args[0];
 
             file = new File(System.getenv("c:\\Users\\kapta\\IdeaProjects\\CSC245_Project2_Secure") + fileName).getCanonicalFile();
             inputStream = new BufferedReader(new FileReader(fileName));
@@ -31,11 +38,42 @@ public class CSC245_Project2 {
         } catch (IOException x) {
             System.out.println("Invalid File");
         }
-//        try {
-//            FileInputStream fileInputStream = new FileInputStream(file);
-//        } catch (FileNotFoundException x) {
-//            System.out.println("Invalid file");
-//        }
+
+        try {
+            inputStream = new BufferedReader(new FileReader(filename));
+
+            System.out.println("Email Addresses:");
+            // Read one Line using BufferedReader
+            while ((fileLine = inputStream.readLine()) != null) {
+                // Normalize data
+                fileLine = Normalizer.normalize(fileLine, Normalizer.Form.NFKC);
+
+                // Sanitize each character before printing
+                StringBuilder line = new StringBuilder(fileLine.length());
+                for(int i = 0; i < fileLine.length(); i++) {
+                    char x = fileLine.charAt(i);
+                    if(Character.isLetterOrDigit(x) || x == ' ' || x == '\'') {
+                        line.append(x);
+                    }
+                }
+                fileLine = line.toString();
+
+                System.out.println(fileLine);
+            }
+        } catch (IOException io) {
+            System.out.println("File IO exception" + io.getMessage());
+        } finally {
+            // Need another catch for closing
+            // the streams
+            try {
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+            } catch (IOException io) {
+                System.out.println("Issue closing the Files" + io.getMessage());
+            }
+
+        }
     }
 
 }
